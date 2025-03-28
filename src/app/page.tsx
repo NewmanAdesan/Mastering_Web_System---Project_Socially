ef6dad6b9b72c9b61368f729b8af7fb532ee39d7
NewmanAdesan <newmanadesan@gmail.com>
2025-03-28 18:44:30 +0100

    feat(posts): add authenticated post creation UI
    - Conditionally renders create post interface when Clerk user is authenticated
    - Includes basic form layout and submission framework
    - Sets up initial state management for post content and post phto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import CreatePost from "@/components/CreatePost";
 import ModalToggle from "@/components/ModalToggle";
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default async function Home() {
+export default async function Home() { 
+  const user = await currentUser();
   return (
-    <div>
-      <h1>Home page content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+          {user ? <CreatePost /> : null}
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky">
+          WhoToFollow
+      </div>
     </div>
   );
 }
