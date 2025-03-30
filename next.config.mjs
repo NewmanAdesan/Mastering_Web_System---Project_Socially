53693436d8cb13c25219af90f10b8d9a8e2f81e7
NewmanAdesan <newmanadesan@gmail.com>
2025-03-30 10:30:38 +0100

    fix: update next config for vercel, in order to fix vercel deployment 'Error: Failed to collect page data for /_not-found'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone', // Required for Vercel deployments
+    experimental: {
+      missingSuspenseWithCSRBailout: false, // Disables strict not-found checks
+    },
+    // Optional but recommended for App Router:
+    skipTrailingSlashRedirect: true,
+    skipMiddlewareUrlNormalize: true
+};
 
 export default nextConfig;
